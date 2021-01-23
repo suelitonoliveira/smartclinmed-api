e038cc8c25c8f6a27ef0d06cde531e287e85e051
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-01-23 09:50:16 -0300

    Projeto Criado

---
+++ src/main/java/br/com/smarclinmed/web/SmarclinmedApplication.java
@@ -0,0 +1,13 @@
+package br.com.smarclinmed.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarclinmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarclinmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/smarclinmed/web/SmarclinmedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smarclinmed.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarclinmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
