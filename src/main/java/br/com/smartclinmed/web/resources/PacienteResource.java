75a338ec11e109fd8b25f2028b8b27154797fe30
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-01-26 12:35:50 -0300

    CRUD Paciente

---
+++ src/main/java/br/com/smartclinmed/web/resources/PacienteResource.java
@@ -0,0 +1,58 @@
+package br.com.smartclinmed.web.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.smartclinmed.web.domain.Paciente;
+import br.com.smartclinmed.web.services.PacienteService;
+
+@RestController
+@RequestMapping(value = "/pacientes")
+public class PacienteResource {
+	@Autowired
+	private PacienteService service;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Paciente>> findAll() {
+		List<Paciente> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Paciente> find(@PathVariable Long id) {
+		Paciente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Paciente obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Paciente obj, @PathVariable Long id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ src/main/java/br/com/smartclinmed/web/services/PacienteService.java
@@ -0,0 +1,69 @@
+package br.com.smartclinmed.web.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.smartclinmed.web.domain.Paciente;
+import br.com.smartclinmed.web.repositories.PacienteRepository;
+
+@Service
+public class PacienteService {
+	
+	@Autowired
+	private PacienteRepository repo;
+
+	public Paciente find(Long id) {
+		Optional<Paciente> obj = repo.findById(id);
+		/*
+		 * if (obj.isEmpty()) { throw new
+		 * ObjectNotFoundException("Objeto não encontrato! ID: " + id + ", Tipo: " +
+		 * Paciente.class.getName()); }
+		 */
+		return obj.orElse(null);  //.orElseThrow(() -> new ObjectNotFoundException("Not found"));
+	}
+	
+	public Paciente insert(Paciente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Paciente update(Paciente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			//throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos associados");
+		}
+
+	}
+	
+	public List<Paciente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Paciente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	/*
+	 * public Paciente fromDTO(PacienteDTO objDto) { return new
+	 * Paciente(objDto.getId(), objDto.getNome()); }
+	 */
+	
+}
