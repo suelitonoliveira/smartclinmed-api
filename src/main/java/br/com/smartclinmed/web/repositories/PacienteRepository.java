d0da9114b47c6878fd0d55d4b25ae52905b26398
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-01-30 13:19:14 -0300

    Ajuste em Paciente

--- src/main/java/br/com/smartclinmed/web/repositories/PacienteRepository.java
+++ src/main/java/br/com/smartclinmed/web/repositories/PacienteRepository.java
@@ -1,13 +1,27 @@
 package br.com.smartclinmed.web.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import br.com.smartclinmed.web.domain.Paciente;
+import br.com.smartclinmed.web.domain.software.Inquilino;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 	
+	@Transactional(readOnly = true)
+	Paciente findByIdAndInquilino(Long id, Inquilino inquilino);
 	
+	@Transactional(readOnly = true)
+	List<Paciente> findByInquilino(Inquilino inquilino);
+
+	@Transactional(readOnly = true)
+	Page<Paciente> findByInquilino(Pageable pageable, Inquilino inquilino);
 
 }
--- src/main/java/br/com/smartclinmed/web/services/PacienteService.java
+++ src/main/java/br/com/smartclinmed/web/services/PacienteService.java
@@ -14,58 +14,61 @@ import org.springframework.stereotype.Service;
 
 import br.com.smartclinmed.web.domain.Paciente;
 import br.com.smartclinmed.web.repositories.PacienteRepository;
+import br.com.smartclinmed.web.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PacienteService {
-	
+
 	@Autowired
 	private PacienteRepository repo;
 
 	public Paciente find(Long id) {
 		Optional<Paciente> obj = repo.findById(id);
-		
-		
-		/*
-		 * if (obj.isEmpty()) { throw new
-		 * ObjectNotFoundException("Objeto não encontrato! ID: " + id + ", Tipo: " +
-		 * Paciente.class.getName()); }
-		 */
-		return obj.orElse(null);  //.orElseThrow(() -> new ObjectNotFoundException("Not found"));
+
+		if (obj.isEmpty()) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrato! ID: " + id + ", Tipo: " + Paciente.class.getName());
+		}
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Not found"));
 	}
+
 	@Transactional
 	public Paciente insert(Paciente obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+
 	@Transactional
 	public Paciente update(Paciente obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	@Transactional
 	public void delete(Long id) {
 		find(id);
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			//throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos associados");
+			// throw new DataIntegrityException("Não é possivel excluir uma categoria com
+			// produtos associados");
 		}
 
 	}
-	
-	public List<Paciente> findAll(){
+
+	public List<Paciente> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Paciente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Paciente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	/*
 	 * public Paciente fromDTO(PacienteDTO objDto) { return new
 	 * Paciente(objDto.getId(), objDto.getNome()); }
 	 */
-	
+
 }
