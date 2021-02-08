c86eee7e3048b25f28a987a852cead321d549134
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-02-08 14:51:50 -0300

    ajustes tentando deploy

--- src/main/java/br/com/smartclinmed/web/config/TestConfig.java
+++ src/main/java/br/com/smartclinmed/web/config/TestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.smartclinmed.web.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 
 	@Autowired
