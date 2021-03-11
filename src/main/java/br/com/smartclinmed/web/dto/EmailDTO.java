ccb7128f0d3e3c0d1c9c04a5845813b89eec959c
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-03-11 17:25:35 -0300

    Ajuste emailDTO

--- src/main/java/br/com/smartclinmed/web/dto/EmailDTO.java
+++ src/main/java/br/com/smartclinmed/web/dto/EmailDTO.java
@@ -8,8 +8,8 @@ import javax.validation.constraints.NotEmpty;
 public class EmailDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@NotEmpty(message = "Mandatory filling")
-	@Email(message = "Invalid email")
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
 
 	public EmailDTO() {
