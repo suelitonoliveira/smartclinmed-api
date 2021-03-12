c2266a9a2267a5bc0f8b6e1e5ac20413fea7219b
CarlosHenrique414 <xideiafizzx@gmail.com>
2021-03-12 00:28:24 -0300

    Implementando-Inserção-Deleção-Atualização-em-API-Especialidades

--- src/main/java/br/com/smartclinmed/web/services/EspecialidadeService.java
+++ src/main/java/br/com/smartclinmed/web/services/EspecialidadeService.java
@@ -3,7 +3,7 @@ package br.com.smartclinmed.web.services;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
